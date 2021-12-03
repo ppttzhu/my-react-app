1a36e1542b8c76bc4cba4cbbe19dda98a2e6ce1f
Haley Peng <yixinp@amazon.com>
2021-12-03 10:42:12 -0600

    finished section 3

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,23 @@
+import "./Expenses.css";
+
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+      />
+      <ExpenseItem
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
