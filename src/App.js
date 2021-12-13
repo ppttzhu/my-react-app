64eec9b401475b74d2ba7491205ff040fa533df5
Yixin Peng <ypeng27@hawk.iit.edu>
2021-12-12 18:07:45 -0600

    init ReactMeal

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-import React, { useState } from "react";
-
-import AuthContext from "./store/auth-context";
-import Home from "./components/Home/Home";
-import Login from "./components/Login/Login";
-import MainHeader from "./components/MainHeader/MainHeader";
-
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
-
   return (
-    <AuthContext.Provider
-      value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }}
-    >
-      <MainHeader onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </AuthContext.Provider>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
